a59ae9ed42a1f259d0e03a1c070ed522aeecfca3
bragegsa <bragegsa@stud.ntnu.no>
2022-04-19 15:27:36 +0200

    Update 1

--- Assignment 3/Assignment 3/problem2_template.py
+++ Assignment 3/Assignment 3/problem2_template.py
@@ -6,17 +6,35 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
+import numpy as np
 
 transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5),
-                                                     (0.5, 0.5, 0.5))])
+                                transforms.Normalize(mean=(0.5, 0.5, 0.5),
+                                                     std=(0.5, 0.5, 0.5))])
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 kwargs = {} if device=='cpu' else {'num_workers': 1, 'pin_memory': True}
 batch_size=4
 
+mean = (0.5, 0.5, 0.5)
+std = (.25, .25, .25)
+
+transform_train_aug = transforms.Compose([
+	    transforms.RandomHorizontalFlip(), 
+        transforms.ToTensor(), 
+        transforms.Normalize(mean, std)])
+
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
+
+# We added som data augmentation
+trainset_aug = torchvision.datasets.CIFAR10(root='./data',
+                                  train=True,
+                                  download=True,
+                                  transform=transform_train_aug)
+
+# Here we expand our trainingset with our horizontaly flipped dataset
+trainset = torch.utils.data.ConcatDataset((trainset, trainset_aug))
+
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, **kwargs)
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
@@ -30,17 +48,35 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+
+        # CNN
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=16, kernel_size=3, padding=1)
+
+        # DNN
+        self.fc1 = nn.Linear(in_features=16*8*8, out_features=120)
+
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+
+        self.fc3 = nn.Linear(in_features=84, out_features=10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        # x = F.relu(self.conv3(x))
+        # x = F.relu(self.conv4(x))
+        # print(x.size())
+        x = x.view(-1, 16 * 8 * 8)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -53,7 +89,9 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(5):  # loop over the dataset multiple times
+    epochs = 10
+
+    for epoch in range(epochs):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
