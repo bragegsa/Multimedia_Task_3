9e8c7e996d6104b171d2f3315bbeee1bc156bfa9
Niklad <60095505+Niklad@users.noreply.github.com>
2022-04-21 23:20:35 +0200

    WIP uni quantizer

--- Assignment 3/Assignment 3/problem1_e.py
+++ Assignment 3/Assignment 3/problem1_e.py
@@ -1,20 +1,32 @@
-''' Quantizing '''
+"""Quantizing, baby"""
 
 import numpy as np
 
-RANDOM_INPUT = np.round(np.random.rand(10) * 10, 1)
+
+SAMPLES_MEAN = 0
+SAMPLES_STD = 1
+SAMPLES_SIZE = 10
+SAMPLES = np.random.normal(loc=SAMPLES_MEAN, scale=SAMPLES_STD, size=SAMPLES_SIZE)
+
 DELTA = int('00001111', 2)
-DELTA = 1
 RANGE = 10
 
-def unform_quantizer(input_values, delta):
-    """Quanitzing input values uniformely"""
+def unform_quantizer(samples, num_of_quantization_levels, quantization_range):
+    """Quantzing input values uniformely"""
 
-    # value_of_nearest_level = quantization_levels[np.argmin(np.abs(input_value-quantization_levels))]
+    delta = np.linspace(start=quantization_range[0], stop=quantization_range[1], num=num_of_quantization_levels)
+    values_of_nearest_level = delta * np.floor(samples/delta + 1/2)
 
-    value_of_nearest_level = delta * np.floor(input_values/delta + 1/2)
+    bits_per_sample = np.log2(num_of_quantization_levels)
 
-    return value_of_nearest_level
+    return values_of_nearest_level, bits_per_sample
 
 
-print(RANDOM_INPUT, "is quantized to", unform_quantizer(input_values=RANDOM_INPUT, delta=DELTA))
+values_of_nearest_level_1, bits_per_sample_1 = unform_quantizer(samples=SAMPLES, num_of_quantization_levels=2, quantization_range=(0,1))
+# values_of_nearest_level_2, bits_per_sample_2 = unform_quantizer(samples=SAMPLES, num_of_quantization_levels=4, quantization_range=10)
+# values_of_nearest_level_3, bits_per_sample_3 = unform_quantizer(samples=SAMPLES, num_of_quantization_levels=8, quantization_range=10)
+
+print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.\n")
+# print(SAMPLES, "are quantized to", values_of_nearest_level_2, "with", bits_per_sample_2, "bits per sample.\n")
+# print(SAMPLES, "are quantized to", values_of_nearest_level_3, "with", bits_per_sample_3, "bits per sample.\n")
+
