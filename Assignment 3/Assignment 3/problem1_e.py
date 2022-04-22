1d8e6319358edf7be45dd2f4aec0c832593c3c99
Niklad <60095505+Niklad@users.noreply.github.com>
2022-04-22 15:42:24 +0200

    Printing more nicely

--- Assignment 3/Assignment 3/problem1_e.py
+++ Assignment 3/Assignment 3/problem1_e.py
@@ -7,7 +7,7 @@ import numpy as np
 SAMPLES_MEAN = 0
 SAMPLES_STD = 1
 SAMPLES_SIZE = 10
-SAMPLES = np.random.normal(loc=SAMPLES_MEAN, scale=SAMPLES_STD, size=SAMPLES_SIZE)
+SAMPLES = np.round(np.random.normal(loc=SAMPLES_MEAN, scale=SAMPLES_STD, size=SAMPLES_SIZE), decimals=3)
 
 
 def uniform_quantizer(samples, bits_per_sample, quantization_range):
@@ -17,32 +17,29 @@ def uniform_quantizer(samples, bits_per_sample, quantization_range):
     i = np.clip(np.round((samples-quantization_range[0]) / delta), a_min=quantization_range[0], a_max=quantization_range[1])
     values_of_nearest_level = quantization_range[0] + i*delta
 
-    return values_of_nearest_level, bits_per_sample
+    return np.round(values_of_nearest_level, decimals=3), bits_per_sample
 
 
-# From 1 to 3 bits per sample in the range -3 to 3
-values_of_nearest_level_1, bits_per_sample_1 = uniform_quantizer(samples=SAMPLES, bits_per_sample=1, quantization_range=(-3,3))
-values_of_nearest_level_2, bits_per_sample_2 = uniform_quantizer(samples=SAMPLES, bits_per_sample=2, quantization_range=(-3,3))
-values_of_nearest_level_3, bits_per_sample_3 = uniform_quantizer(samples=SAMPLES, bits_per_sample=3, quantization_range=(-3,3))
+def find_distortion(input_values, quantized_values):
 
-print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.\n")
-print(SAMPLES, "are quantized to", values_of_nearest_level_2, "with", bits_per_sample_2, "bits per sample.\n")
-print(SAMPLES, "are quantized to", values_of_nearest_level_3, "with", bits_per_sample_3, "bits per sample.\n")
+    distortion = np.mean((input_values-quantized_values)**2)
 
-# From 1 to 3 bits per sample in the range -4 to 4
-values_of_nearest_level_1, bits_per_sample_1 = uniform_quantizer(samples=SAMPLES, bits_per_sample=1, quantization_range=(-4,4))
-values_of_nearest_level_2, bits_per_sample_2 = uniform_quantizer(samples=SAMPLES, bits_per_sample=2, quantization_range=(-4,4))
-values_of_nearest_level_3, bits_per_sample_3 = uniform_quantizer(samples=SAMPLES, bits_per_sample=3, quantization_range=(-4,4))
+    return np.round(distortion, decimals=2)
 
-print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.\n")
-print(SAMPLES, "are quantized to", values_of_nearest_level_2, "with", bits_per_sample_2, "bits per sample.\n")
-print(SAMPLES, "are quantized to", values_of_nearest_level_3, "with", bits_per_sample_3, "bits per sample.\n")
 
-# From 1 to 3 bits per sample in the range -2 to 2
-values_of_nearest_level_1, bits_per_sample_1 = uniform_quantizer(samples=SAMPLES, bits_per_sample=1, quantization_range=(-2,2))
-values_of_nearest_level_2, bits_per_sample_2 = uniform_quantizer(samples=SAMPLES, bits_per_sample=2, quantization_range=(-2,2))
-values_of_nearest_level_3, bits_per_sample_3 = uniform_quantizer(samples=SAMPLES, bits_per_sample=3, quantization_range=(-2,2))
+def print_quantization_results(x):
 
-print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.\n")
-print(SAMPLES, "are quantized to", values_of_nearest_level_2, "with", bits_per_sample_2, "bits per sample.\n")
-print(SAMPLES, "are quantized to", values_of_nearest_level_3, "with", bits_per_sample_3, "bits per sample.\n")
+    for r in range(5,1,-1):
+        
+        print("From 1 to 3 bits per sample in the range", -r, "to", r, "\n")
+        
+        for bitrate in range(1,4):
+            values_of_nearest_level_1, bits_per_sample_1 = uniform_quantizer(samples=x, bits_per_sample=bitrate, quantization_range=(-r,r))
+
+            print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.")
+            print("The distortion is", find_distortion(input_values=x, quantized_values=values_of_nearest_level_1), "\n")
+
+        print("\n")
+
+
+print_quantization_results(x=SAMPLES)
