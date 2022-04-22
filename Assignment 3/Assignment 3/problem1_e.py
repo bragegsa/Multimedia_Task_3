f72d2b35acea284e85a32adf166ef33d6d63baae
Niklad <60095505+Niklad@users.noreply.github.com>
2022-04-22 10:22:08 +0200

    1d should be good, with some printing

--- Assignment 3/Assignment 3/problem1_e.py
+++ Assignment 3/Assignment 3/problem1_e.py
@@ -3,30 +3,46 @@
 import numpy as np
 
 
+# Generate samples from normal distribution
 SAMPLES_MEAN = 0
 SAMPLES_STD = 1
 SAMPLES_SIZE = 10
 SAMPLES = np.random.normal(loc=SAMPLES_MEAN, scale=SAMPLES_STD, size=SAMPLES_SIZE)
 
-DELTA = int('00001111', 2)
-RANGE = 10
 
-def unform_quantizer(samples, num_of_quantization_levels, quantization_range):
+def uniform_quantizer(samples, bits_per_sample, quantization_range):
     """Quantzing input values uniformely"""
 
-    delta = np.linspace(start=quantization_range[0], stop=quantization_range[1], num=num_of_quantization_levels)
-    values_of_nearest_level = delta * np.floor(samples/delta + 1/2)
-
-    bits_per_sample = np.log2(num_of_quantization_levels)
+    delta = (quantization_range[1]-quantization_range[0]) / (2**bits_per_sample-1)
+    i = np.clip(np.round((samples-quantization_range[0]) / delta), a_min=quantization_range[0], a_max=quantization_range[1])
+    values_of_nearest_level = quantization_range[0] + i*delta
 
     return values_of_nearest_level, bits_per_sample
 
 
-values_of_nearest_level_1, bits_per_sample_1 = unform_quantizer(samples=SAMPLES, num_of_quantization_levels=2, quantization_range=(0,1))
-# values_of_nearest_level_2, bits_per_sample_2 = unform_quantizer(samples=SAMPLES, num_of_quantization_levels=4, quantization_range=10)
-# values_of_nearest_level_3, bits_per_sample_3 = unform_quantizer(samples=SAMPLES, num_of_quantization_levels=8, quantization_range=10)
+# From 1 to 3 bits per sample in the range -3 to 3
+values_of_nearest_level_1, bits_per_sample_1 = uniform_quantizer(samples=SAMPLES, bits_per_sample=1, quantization_range=(-3,3))
+values_of_nearest_level_2, bits_per_sample_2 = uniform_quantizer(samples=SAMPLES, bits_per_sample=2, quantization_range=(-3,3))
+values_of_nearest_level_3, bits_per_sample_3 = uniform_quantizer(samples=SAMPLES, bits_per_sample=3, quantization_range=(-3,3))
 
 print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.\n")
-# print(SAMPLES, "are quantized to", values_of_nearest_level_2, "with", bits_per_sample_2, "bits per sample.\n")
-# print(SAMPLES, "are quantized to", values_of_nearest_level_3, "with", bits_per_sample_3, "bits per sample.\n")
+print(SAMPLES, "are quantized to", values_of_nearest_level_2, "with", bits_per_sample_2, "bits per sample.\n")
+print(SAMPLES, "are quantized to", values_of_nearest_level_3, "with", bits_per_sample_3, "bits per sample.\n")
 
+# From 1 to 3 bits per sample in the range -4 to 4
+values_of_nearest_level_1, bits_per_sample_1 = uniform_quantizer(samples=SAMPLES, bits_per_sample=1, quantization_range=(-4,4))
+values_of_nearest_level_2, bits_per_sample_2 = uniform_quantizer(samples=SAMPLES, bits_per_sample=2, quantization_range=(-4,4))
+values_of_nearest_level_3, bits_per_sample_3 = uniform_quantizer(samples=SAMPLES, bits_per_sample=3, quantization_range=(-4,4))
+
+print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.\n")
+print(SAMPLES, "are quantized to", values_of_nearest_level_2, "with", bits_per_sample_2, "bits per sample.\n")
+print(SAMPLES, "are quantized to", values_of_nearest_level_3, "with", bits_per_sample_3, "bits per sample.\n")
+
+# From 1 to 3 bits per sample in the range -2 to 2
+values_of_nearest_level_1, bits_per_sample_1 = uniform_quantizer(samples=SAMPLES, bits_per_sample=1, quantization_range=(-2,2))
+values_of_nearest_level_2, bits_per_sample_2 = uniform_quantizer(samples=SAMPLES, bits_per_sample=2, quantization_range=(-2,2))
+values_of_nearest_level_3, bits_per_sample_3 = uniform_quantizer(samples=SAMPLES, bits_per_sample=3, quantization_range=(-2,2))
+
+print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.\n")
+print(SAMPLES, "are quantized to", values_of_nearest_level_2, "with", bits_per_sample_2, "bits per sample.\n")
+print(SAMPLES, "are quantized to", values_of_nearest_level_3, "with", bits_per_sample_3, "bits per sample.\n")
