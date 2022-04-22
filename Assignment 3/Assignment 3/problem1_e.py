d90f97590706cc934d5ac1c74a8798658d396f43
Niklad <60095505+Niklad@users.noreply.github.com>
2022-04-22 17:03:53 +0200

    Gettting closer to shit working
    Co-authored-by: Brage Gaasø Samsonsen <bragegsa@users.noreply.github.com>

--- Assignment 3/Assignment 3/problem1_e.py
+++ Assignment 3/Assignment 3/problem1_e.py
@@ -1,6 +1,7 @@
 """Quantizing, baby"""
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Generate samples from normal distribution
@@ -27,19 +28,46 @@ def find_distortion(input_values, quantized_values):
     return np.round(distortion, decimals=2)
 
 
+def find_rate_distortion_function(sample_std):
+    """Calculate rate-distortion"""
+
+    distortion = np.linspace(start=0.001, stop=2*sample_std, num=1000)
+    rate_distortion_function = 1/2 * np.log2(sample_std**2 / distortion)
+
+    return distortion, np.maximum(rate_distortion_function, 0)
+
+
 def print_quantization_results(x):
+    """Print and plot quantization results"""
+
+    source, rate_distortion_function_source = find_rate_distortion_function(SAMPLES_STD)
+
+    distortions = np.array([])
+    rates = np.array([])
+
+    plt.figure()
+    plt.plot(source, rate_distortion_function_source)
+    plt.grid()
 
     for r in range(5,1,-1):
         
         print("From 1 to 3 bits per sample in the range", -r, "to", r, "\n")
+        print(x, "are quantized to \n")
         
         for bitrate in range(1,4):
-            values_of_nearest_level_1, bits_per_sample_1 = uniform_quantizer(samples=x, bits_per_sample=bitrate, quantization_range=(-r,r))
+            values_of_nearest_level, bits_per_sample = uniform_quantizer(samples=x, bits_per_sample=bitrate, quantization_range=(-r,r))
 
-            print(SAMPLES, "are quantized to", values_of_nearest_level_1, "with", bits_per_sample_1, "bits per sample.")
-            print("The distortion is", find_distortion(input_values=x, quantized_values=values_of_nearest_level_1), "\n")
+            print(values_of_nearest_level, "with", bits_per_sample, "bits per sample.")
+            print("The distortion is", find_distortion(input_values=x, quantized_values=values_of_nearest_level), "\n")
 
+            distortions = np.append(distortions, find_distortion(input_values=x, quantized_values=values_of_nearest_level))
+            rates = np.append(rates, bits_per_sample)
+            
         print("\n")
+    
+    # for i in range(len(distortions)):
+    #     plt.plot(distortions[i], rates[i])
+    #     plt.show()
 
 
 print_quantization_results(x=SAMPLES)
